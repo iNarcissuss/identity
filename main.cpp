c698e42856acef5c8b761b452c26a15c47ba029e
Charis Kouzinopoulos <kouzinopoulos@.iti.gr>
2018-07-05 10:48:21 +0300

    Finished integration with PcapPlusPlus

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ extern "C" {
 #include "ac/ac.h"
 }
 
+#include <array>
 #include <cstring>
 #include <fstream>
 #include <iostream>
@@ -15,11 +16,17 @@ extern "C" {
 #include <arpa/inet.h>
 #include <pcapplusplus/PcapFileDevice.h>
 #include <pcapplusplus/Packet.h>
+#include <pcapplusplus/ArpLayer.h>
+#include <pcapplusplus/DnsLayer.h>
 #include <pcapplusplus/EthLayer.h>
+#include <pcapplusplus/IcmpLayer.h>
 #include <pcapplusplus/IPv4Layer.h>
 #include <pcapplusplus/TcpLayer.h>
+#include <pcapplusplus/UdpLayer.h>
 #include <pcapplusplus/HttpLayer.h>
+#include <pcapplusplus/PayloadLayer.h>
 
+using std::array;
 using std::cout;
 using std::endl;
 using std::ifstream;
@@ -28,6 +35,9 @@ using std::stringstream;
 using std::vector;
 
 struct Frame {
+	Frame(string id)
+    : mId(id){};
+
   Frame(string id, string timestamp, string source_ip, string source_mac, string dest_ip, string dest_mac,
         string length, string protocol, string source_port, string dest_port, string payload)
     : mId(id),
@@ -49,9 +59,13 @@ struct Frame {
   string mDest_mac;
   string mLength;
   string mProtocol;
-  string mSource_port;
-  string mDest_port;
-  string mPayload;
+  string mSource_port = "";
+  string mDest_port = "";
+  string mPayload = "";
+
+  unsigned int mTotalLength = 0;
+  unsigned int mHeaderLength;
+  unsigned int mPayloadLength;
 };
 
 class Credentials {
@@ -71,6 +85,207 @@ struct Results multiac(unsigned char** pattern, int m, unsigned char* text, int 
   free_ac(table, alphabet);
 
   return results;
+}
+
+std::string getProtocolTypeAsString(pcpp::ProtocolType protocolType)
+{
+	switch (protocolType)
+	{
+	case pcpp::Ethernet:
+		return "Ethernet";
+	case pcpp::IPv4:
+		return "IPv4";
+	case pcpp::TCP:
+		return "TCP";
+	case pcpp::UDP:
+		return "UDP";
+	case pcpp::ARP:
+		return "ARP";
+	case pcpp::DNS:
+		return "DNS";
+	case pcpp::HTTPRequest:
+	case pcpp::HTTPResponse:
+		return "HTTP";
+	default:
+		return "Unknown";
+	}
+}
+
+std::string printTcpFlags(pcpp::TcpLayer* tcpLayer)
+{
+	std::string result = "";
+	if (tcpLayer->getTcpHeader()->synFlag == 1)
+		result += "SYN ";
+	if (tcpLayer->getTcpHeader()->ackFlag == 1)
+		result += "ACK ";
+	if (tcpLayer->getTcpHeader()->pshFlag == 1)
+		result += "PSH ";
+	if (tcpLayer->getTcpHeader()->cwrFlag == 1)
+		result += "CWR ";
+	if (tcpLayer->getTcpHeader()->urgFlag == 1)
+		result += "URG ";
+	if (tcpLayer->getTcpHeader()->eceFlag == 1)
+		result += "ECE ";
+	if (tcpLayer->getTcpHeader()->rstFlag == 1)
+		result += "RST ";
+	if (tcpLayer->getTcpHeader()->finFlag == 1)
+		result += "FIN ";
+
+	return result;
+}
+
+void pcapParser(vector<Frame>& frameVector, const string& filename)
+{
+	pcpp::IFileReaderDevice* reader = pcpp::IFileReaderDevice::getReader(filename.c_str());
+
+  // verify that a reader interface was indeed created
+  if (reader == NULL) {
+	  cout << "Cannot determine pcap reader for file type" << endl;
+	  exit(1);
+  }
+
+  // open the reader for reading
+  if (!reader->open()) {
+    printf("Cannot open pcap file for reading\n");
+    exit(1);
+  }
+
+  // Parse all packets in pcap file
+  unsigned int packetCounter = 0;
+  while(1) {
+    // cout << "Packet " << packetCounter << endl;
+
+	  pcpp::RawPacket rawPacket;
+	  // Break the loop when a next file could not be found
+		if (!reader->getNextPacket(rawPacket)) {
+			break;
+		}
+
+		// Create new frame
+		Frame frame(std::to_string(packetCounter));
+		packetCounter++;
+
+		// parse the raw packet into a parsed packet
+		pcpp::Packet parsedPacket(&rawPacket);
+
+		// first let's go over the layers one by one and find out its type, its total length, its header length and its payload length
+		for (pcpp::Layer* curLayer = parsedPacket.getFirstLayer(); curLayer != NULL; curLayer = curLayer->getNextLayer()) {
+			// printf("%i] Layer type: %s %i; Total data: %d [bytes]; Layer data: %d [bytes]; Layer payload: %d [bytes]\n", packetCounter,
+			// 		getProtocolTypeAsString(curLayer->getProtocol()).c_str(), // get layer type
+			// 		curLayer->getProtocol(),
+			// 		(int)curLayer->getDataLen(),                              // get total length of the layer
+			// 		(int)curLayer->getHeaderLen(),                            // get the header length of the layer
+			// 		(int)curLayer->getLayerPayloadSize());                    // get the payload length of the layer (equals total length minus header length)
+
+			frame.mTotalLength += (int)curLayer->getDataLen();
+		}
+
+		pcpp::IPv4Layer* ipLayer = parsedPacket.getLayerOfType<pcpp::IPv4Layer>();
+		if (ipLayer != NULL) {
+			// printf("\nSource IP address: %s\n", ipLayer->getSrcIpAddress().toString().c_str());
+			// printf("Destination IP address: %s\n", ipLayer->getDstIpAddress().toString().c_str());
+			// printf("IP ID: 0x%X\n", ntohs(ipLayer->getIPv4Header()->ipId));
+			// printf("TTL: %d\n", ipLayer->getIPv4Header()->timeToLive);
+
+			frame.mSource_ip = ipLayer->getSrcIpAddress().toString().c_str();
+			frame.mDest_ip = ipLayer->getDstIpAddress().toString().c_str();
+		}
+
+		pcpp::DnsLayer* dnsLayer = parsedPacket.getLayerOfType<pcpp::DnsLayer>();
+		if (dnsLayer != NULL) {
+			// printf("\nSource IP address: %s\n", ipLayer->getSrcIpAddress().toString().c_str());
+			// printf("Destination IP address: %s\n", ipLayer->getDstIpAddress().toString().c_str());
+			// printf("IP ID: 0x%X\n", ntohs(ipLayer->getIPv4Header()->ipId));
+			// printf("TTL: %d\n", ipLayer->getIPv4Header()->timeToLive);
+
+			frame.mProtocol = "DNS";
+		}
+
+		pcpp::TcpLayer* tcpLayer = parsedPacket.getLayerOfType<pcpp::TcpLayer>();
+		if (tcpLayer != NULL) {
+			// printf("\nSource TCP port: %d\n", (int)ntohs(tcpLayer->getTcpHeader()->portSrc));
+			// printf("Destination TCP port: %d\n", (int)ntohs(tcpLayer->getTcpHeader()->portDst));
+			// printf("Window size: %d\n", (int)ntohs(tcpLayer->getTcpHeader()->windowSize));
+			// printf("TCP flags: %s\n", printTcpFlags(tcpLayer).c_str());
+
+			frame.mSource_port = (int)ntohs(tcpLayer->getTcpHeader()->portSrc);
+			frame.mDest_port = (int)ntohs(tcpLayer->getTcpHeader()->portDst);
+			frame.mProtocol = "TCP";
+		}
+
+		pcpp::UdpLayer* udpLayer = parsedPacket.getLayerOfType<pcpp::UdpLayer>();
+		if (udpLayer != NULL) {
+			frame.mSource_port = (int)ntohs(udpLayer->getUdpHeader()->portSrc);
+			frame.mDest_port = (int)ntohs(udpLayer->getUdpHeader()->portDst);
+			frame.mProtocol = "UDP";
+		}
+
+		pcpp::IcmpLayer* icmpLayer = parsedPacket.getLayerOfType<pcpp::IcmpLayer>();
+		if (icmpLayer != NULL) {
+			frame.mProtocol = "ICMP";
+		}
+
+		pcpp::ArpLayer* arpLayer = parsedPacket.getLayerOfType<pcpp::ArpLayer>();
+		if (arpLayer != NULL) {
+			frame.mProtocol = "ARP";
+		}
+
+		pcpp::PayloadLayer* payloadLayer = parsedPacket.getLayerOfType<pcpp::PayloadLayer>();
+		if (payloadLayer != NULL) {
+			//cout << "Payload: " << payloadLayer->getPayloadLen() << " " << payloadLayer->getPayload() << endl;
+			uint8_t payloadArray[payloadLayer->getPayloadLen()];
+
+			std::ostringstream convert;
+			// Sanitize the packet payload
+			for (int i = 0; i < payloadLayer->getPayloadLen(); i++) {
+    		if (payloadLayer->getPayload()[i] >= 32 && payloadLayer->getPayload()[i] <= 126) {
+    			convert << (char)payloadLayer->getPayload()[i];
+    		} else {
+    			convert << '.';
+    		}
+			}
+
+			frame.mPayload = convert.str();
+			// cout << payload << endl;
+		}
+		frameVector.push_back(frame);
+	}
+
+	// Close the file reader, we don't need it anymore
+	reader->close();
+
+	// Statistics!
+	cout << "Parsed " << frameVector.size() << " frames" << endl;
+
+  int udpCounter = 0;
+  int tcpCounter = 0;
+  int icmpCounter = 0;
+  int arpCounter = 0;
+  int unknownCounter = 0;
+  for (auto& i : frameVector) {
+    if (i.mProtocol == "UDP") {
+      udpCounter++;
+    } else if (i.mProtocol == "TCP") {
+      tcpCounter++;
+    } else if (i.mProtocol == "ICMP") {
+      icmpCounter++;
+    } else if (i.mProtocol == "ARP") {
+      arpCounter++;
+    } else {
+      unknownCounter++;
+    }
+  }
+  cout << "Parsed " << udpCounter << " UDP " << tcpCounter << " TCP " << icmpCounter << " ICMP " << arpCounter
+       << " ARP and " << unknownCounter << " unknown protocol messages" << endl;
+
+  double size = 0;
+  for (auto& i : frameVector) {
+    if (i.mProtocol == "TCP") {
+      size += i.mPayload.length();
+    }
+  }
+
+  cout << "Average payload size of all TCP packets: " << size / tcpCounter << " bytes" << endl;
 }
 
 void csvParser(vector<Frame>& frameVector, const string& filename)
@@ -136,33 +351,14 @@ void csvParser(vector<Frame>& frameVector, const string& filename)
   cout << "Average payload size of all TCP packets: " << size / tcpCounter << " bytes" << endl;
 }
 
-std::string getProtocolTypeAsString(pcpp::ProtocolType protocolType)
-{
-	switch (protocolType)
-	{
-	case pcpp::Ethernet:
-		return "Ethernet";
-	case pcpp::IPv4:
-		return "IPv4";
-	case pcpp::TCP:
-		return "TCP";
-	case pcpp::UDP:
-		return "UDP";
-	case pcpp::ARP:
-		return "ARP";
-	case pcpp::HTTPRequest:
-	case pcpp::HTTPResponse:
-		return "HTTP";
-	default:
-		return "Unknown";
-	}
-}
-
 int main(int argc, char** argv)
 {
 	// Parse network payload and store to frameVector
-  vector<Frame> frameVector;
-  csvParser(frameVector, "./charis2.csv");
+  // vector<Frame> frameVector;
+  // csvParser(frameVector, "./charis2.csv");
+
+  vector<Frame> frameVector2;
+  pcapParser(frameVector2, "capture.pcap");
 
   // Parse configuration file with IPs and usernames
   std::ifstream ifs("input.json");
@@ -187,9 +383,9 @@ int main(int argc, char** argv)
 
   cout << "Alert type, Frame ID, Source IP, Destination IP, Username, Frame location" << endl;
   outFile << "Alert type, Frame ID, Source IP, Destination IP, Username, Frame location" << endl;
-  for (auto& it : frameVector) {
+  for (auto& it : frameVector2) {
     // Inspect only TCP packets with actual payload
-    if (it.mProtocol == "TCP" && std::stod(it.mLength) > 96) {
+    if (it.mProtocol == "TCP" && it.mPayload.length() > 0) {
       const int m2 = 7;
       const int alphabet2 = 256;
       const int n2 = it.mPayload.length();
@@ -260,56 +456,14 @@ int main(int argc, char** argv)
   // Close output file
   outFile.close();
 
-  pcpp::IFileReaderDevice* reader = pcpp::IFileReaderDevice::getReader("capture2.pcap");
+  // for (auto& it : frameVector) {
+  // 	cout << it.mId << " " << it.mProtocol << " " << it.mSource_ip << " " << it.mSource_port << " " << it.mDest_ip << " " << it.mDest_port << " " << it.mPayload << endl << endl;
+  // 	if (it.mId == "1270") break;
+  // }
 
-  // verify that a reader interface was indeed created
-  if (reader == NULL) {
-	  cout << "Cannot determine pcap reader for file type" << endl;
-	  exit(1);
-  }
-
-  // open the reader for reading
-  if (!reader->open()) {
-    printf("Cannot open pcap file for reading\n");
-    exit(1);
-  }
-
-  pcpp::RawPacket rawPacket;
-	if (!reader->getNextPacket(rawPacket)) {
-		printf("Couldn't read the first packet in the file\n");
-		exit(1);
-	}
-
-	// close the file reader, we don't need it anymore
-	reader->close();
-
-	// parse the raw packet into a parsed packet
-	pcpp::Packet parsedPacket(&rawPacket);
-
-	// first let's go over the layers one by one and find out its type, its total length, its header length and its payload length
-	for (pcpp::Layer* curLayer = parsedPacket.getFirstLayer(); curLayer != NULL; curLayer = curLayer->getNextLayer())
-	{
-		printf("Layer type: %s; Total data: %d [bytes]; Layer data: %d [bytes]; Layer payload: %d [bytes]\n",
-				getProtocolTypeAsString(curLayer->getProtocol()).c_str(), // get layer type
-				(int)curLayer->getDataLen(),                              // get total length of the layer
-				(int)curLayer->getHeaderLen(),                            // get the header length of the layer
-				(int)curLayer->getLayerPayloadSize());                    // get the payload length of the layer (equals total length minus header length)
-	}
-
-	pcpp::IPv4Layer* ipLayer = parsedPacket.getLayerOfType<pcpp::IPv4Layer>();
-	if (ipLayer == NULL) {
-		printf("Something went wrong, couldn't find IPv4 layer\n");
-	} else {
-		printf("\nSource IP address: %s\n", ipLayer->getSrcIpAddress().toString().c_str());
-		printf("Destination IP address: %s\n", ipLayer->getDstIpAddress().toString().c_str());
-		printf("IP ID: 0x%X\n", ntohs(ipLayer->getIPv4Header()->ipId));
-		printf("TTL: %d\n", ipLayer->getIPv4Header()->timeToLive);
-	}
-
-	pcpp::TcpLayer* tcpLayer = parsedPacket.getLayerOfType<pcpp::TcpLayer>();
-	if (tcpLayer == NULL) {
-		printf("Something went wrong, couldn't find TCP layer\n");
-	}
+  // for (auto& it : frameVector2) {
+  // 	if(it.mProtocol == "TCP")cout << it.mId << " " << it.mProtocol << " " << it.mSource_ip << " " << it.mSource_port << " " << it.mDest_ip << " " << it.mDest_port << " " << it.mPayload << endl << endl;
+  // }
 
   return 0;
 }
